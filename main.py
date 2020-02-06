c521ba4a4f7244852d37a3b2476e27d79fabe5c1
Sounryyy <justtrueserj@gmail.com>
2020-02-06 19:16:06 +0300

    Added field-flag for parse student card users at main

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from PNZGUParser import PNZGUParser
 
 
 def main():
-    login = ''
-    password = ''
-    parser = PNZGUParser(login, password)
+    login = 's517658187'
+    password = 'HzrYKkCB'
+    is_need_parse_employers_with_student_card = True
+
+    parser = PNZGUParser(login, password, is_need_parse_employers_with_student_card)
     parser.start()
+
+
+if __name__ == "__main__":
+    main()
